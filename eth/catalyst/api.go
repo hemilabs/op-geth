c5f6053ea483f87a57f32e3a1cc49f1aa8956c61
ClaytonNorthey92 <clayton.northey@gmail.com>
2025-03-18 12:14:19 -0400

    ensure that block is not nil before calling functions on it as receiver in log line

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -20,9 +20,10 @@ package catalyst
 import (
 	"errors"
 	"fmt"
-	"github.com/ethereum/go-ethereum/consensus"
 	"sync"
 	"time"
+
+	"github.com/ethereum/go-ethereum/consensus"
 
 	"github.com/ethereum/go-ethereum/beacon/engine"
 	"github.com/ethereum/go-ethereum/common"
@@ -245,7 +246,12 @@ func (api *ConsensusAPI) forkchoiceUpdated(update engine.ForkchoiceStateV1, payl
 	// need to either trigger a sync, or to reject this forkchoice update for a
 	// reason.
 	block := api.eth.BlockChain().GetBlockByHash(update.HeadBlockHash)
-	log.Info(fmt.Sprintf("ForkchoiceUpdated command requests we set head to %s @ %d", block.Hash().String(), block.NumberU64()))
+	if block != nil {
+		log.Info(fmt.Sprintf("ForkchoiceUpdated command requests we set head to %s @ %d", block.Hash().String(), block.NumberU64()))
+	} else {
+		log.Info("ForkchoiceUpdated command requests we set head to nil block")
+	}
+
 	if block == nil {
 		// If this block was previously invalidated, keep rejecting it here too
 		if res := api.checkInvalidAncestor(update.HeadBlockHash, update.HeadBlockHash); res != nil {
