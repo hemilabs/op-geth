45880e77a30229d77cb23747737868077a35e9aa
Felföldi Zsolt <zsfelfoldi@gmail.com>
2025-03-24 12:27:11 +0100

    core/filtermaps: fix log indexer init conditions (#31455)
    This PR adds an extra condition to the log indexer initialization in
    order to avoid initializing with block 0 as target head. Previously this
    caused the indexer to initialize without a checkpoint. Later, when the
    real chain head was set, it indexed the entire history, then unindexed
    most of it if only the recent history was supposed to be indexed. Now
    the init only happens when there is an actual synced chain head and
    therefore the index is initialized at the most recent checkpoint and
    only the last year is indexed according to the default parameters.
    During checkpoint initialization the best available checkpoint is also
    checked against the history cutoff point and fails if the indexing would
    have to start from a block older than the cutoff. If initialization
    fails then the indexer reverts to unindexed mode instead of retrying
    because the the failure conditions cannot be expected to recover later.

--- eth/backend.go
+++ eth/backend.go
@@ -356,7 +356,12 @@ func New(stack *node.Node, config *ethconfig.Config, ctx context.Context) (*Ethe
 	}
 	fmConfig := filtermaps.Config{History: config.LogHistory, Disabled: config.LogNoHistory, ExportFileName: config.LogExportCheckpoints}
 	chainView := eth.newChainView(eth.blockchain.CurrentBlock())
-	eth.filterMaps = filtermaps.NewFilterMaps(chainDb, chainView, 0, 0, filtermaps.DefaultParams, fmConfig)
+	historyCutoff := eth.blockchain.HistoryPruningCutoff()
+	var finalBlock uint64
+	if fb := eth.blockchain.CurrentFinalBlock(); fb != nil {
+		finalBlock = fb.Number.Uint64()
+	}
+	eth.filterMaps = filtermaps.NewFilterMaps(chainDb, chainView, historyCutoff, finalBlock, filtermaps.DefaultParams, fmConfig)
 	eth.closeFilterMaps = make(chan chan struct{})
 
 	if chainConfig := eth.blockchain.Config(); chainConfig.Optimism != nil { // config.Genesis.Config.ChainID cannot be used because it's based on CLI flags only, thus default to mainnet L1
