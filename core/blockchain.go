e94fd830f34aae9f251d8c8f05b6e3a33cce7cbe
AL-CT <antoniomartins@protonmail.com>
2025-05-02 15:37:22 +0100

    Better tests and change mutex to RWMutex

--- core/blockchain.go
+++ core/blockchain.go
@@ -332,7 +332,7 @@ type BlockChain struct {
 
 	ctx context.Context
 
-	keystoneMtx sync.Mutex
+	keystoneMtx sync.RWMutex
 }
 
 // getHeaderModeTBCEVMHeader returns the EVM header for which the
@@ -4920,6 +4920,9 @@ func (bc *BlockChain) InsertL2Keystone(l2Keystone hemi.L2Keystone) error {
 }
 
 func (bc *BlockChain) GetMostRecentKeystones(count uint) ([]hemi.L2Keystone, error) {
+	bc.keystoneMtx.RLock()
+	defer bc.keystoneMtx.RUnlock()
+
 	if count > 10 {
 		return nil, errors.New("count too large")
 	}
@@ -4928,5 +4931,15 @@ func (bc *BlockChain) GetMostRecentKeystones(count uint) ([]hemi.L2Keystone, err
 }
 
 func (bc *BlockChain) GetKeystoneAndDescendants(hash []byte, count uint) ([]hemi.L2Keystone, error) {
+	bc.keystoneMtx.RLock()
+	defer bc.keystoneMtx.RUnlock()
+
 	return rawdb.GetKeystoneAndDescendants(bc.db, hash, count)
+}
+
+func (bc *BlockChain) GetKeystoneByAbrevHash(hash []byte) (*hemi.L2Keystone, error) {
+	bc.keystoneMtx.RLock()
+	defer bc.keystoneMtx.RUnlock()
+
+	return rawdb.ReadL2KeystoneByAbrevHash(bc.db, hash)
 }
