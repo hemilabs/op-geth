af97485441064ace6bf4a207d5dd8ae71ca31945
Felix Lange <fjl@twurst.com>
2025-05-07 12:53:45 +0200

    beacon/blsync: fix requests encoding in engine_newPayloadV4 (#31775)
    This fixes an issue where blocks containing CL requests triggered an
    error in the engine API. The encoding of requests used base64 instead of
    hex.

--- beacon/blsync/engineclient.go
+++ beacon/blsync/engineclient.go
@@ -105,7 +105,11 @@ func (ec *engineClient) callNewPayload(fork string, event types.ChainHeadEvent) 
 		method = "engine_newPayloadV4"
 		parentBeaconRoot := event.BeaconHead.ParentRoot
 		blobHashes := collectBlobHashes(event.Block)
-		params = append(params, blobHashes, parentBeaconRoot, event.ExecRequests)
+		hexRequests := make([]hexutil.Bytes, len(event.ExecRequests))
+		for i := range event.ExecRequests {
+			hexRequests[i] = hexutil.Bytes(event.ExecRequests[i])
+		}
+		params = append(params, blobHashes, parentBeaconRoot, hexRequests)
 	case "deneb":
 		method = "engine_newPayloadV3"
 		parentBeaconRoot := event.BeaconHead.ParentRoot
