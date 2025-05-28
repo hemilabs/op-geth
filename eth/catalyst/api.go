c43b77732e3d0f47463655f8b1009a86b156f937
AL-CT <antoniomartins@protonmail.com>
2025-05-28 16:34:28 +0100

    fix context

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -350,10 +350,7 @@ type PopPayout struct {
 	Amount       *big.Int       `json:"amount"`
 }
 
-func (api *ConsensusAPI) PopPayoutsByL2Keystone(abrevHash chainhash.Hash) ([]PopPayout, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
+func (api *ConsensusAPI) PopPayoutsByL2Keystone(ctx context.Context, abrevHash chainhash.Hash) ([]PopPayout, error) {
 	payouts, err := vm.TBCFullNode.KeystoneTxsByL2KeystoneAbrevHash(ctx, abrevHash, defaultPayoutBTCBlockDepth)
 	if err != nil {
 		return nil, err
