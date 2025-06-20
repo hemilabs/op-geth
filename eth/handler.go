614ff721fe89415cb975dd16380936b589b1ab76
ClaytonNorthey92 <clayton.northey@gmail.com>
2025-06-20 10:21:40 -0400

    add trace logging around 'too many peers' error (#46)

--- eth/handler.go
+++ eth/handler.go
@@ -285,6 +285,7 @@ func (h *handler) runEthPeer(peer *eth.Peer, handler eth.Handler) error {
 	// Ignore maxPeers if this is a trusted peer
 	if !peer.Peer.Info().Network.Trusted {
 		if reject || h.peers.len() >= h.maxPeers {
+			log.Trace(p2p.DiscTooManyPeers.String(), "reject", reject, "h.peers.len()", h.peers.len(), "h.maxPeers", h.maxPeers)
 			return p2p.DiscTooManyPeers
 		}
 	}
