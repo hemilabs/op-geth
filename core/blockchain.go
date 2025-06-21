696c13d212a0b35e7bef1c5b76829452fb174bdd
rjl493456442 <garyrong0905@gmail.com>
2025-06-21 12:58:04 +0800

    core/state: expose the state reader stats (#31998)
    This pull request introduces a mechanism to expose statistics from the
    state reader, specifically related to cache utilization during state prefetching.
    
    To improve state access performance, a pair of state readers is constructed 
    with a shared local cache. One reader to execute transactions  ahead of time
    to warm up the cache. The other reader is used by the actual chain processing 
    logic, which can benefit from the prefetched states.
    
    This PR adds visibility into how effective the cache is by exposing relevant 
    usage statistics.
    
    ---------
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    Co-authored-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- core/blockchain.go
+++ core/blockchain.go
@@ -3911,18 +3911,32 @@ func (bc *BlockChain) processBlock(parentRoot common.Hash, block *types.Block, s
 		//
 		// Note: the main processor and prefetcher share the same reader with a local
 		// cache for mitigating the overhead of state access.
-		reader, err := bc.statedb.ReaderWithCache(parentRoot)
+		prefetch, process, err := bc.statedb.ReadersWithCacheStats(parentRoot)
 		if err != nil {
 			return nil, err
 		}
-		throwaway, err := state.NewWithReader(parentRoot, bc.statedb, reader)
+		throwaway, err := state.NewWithReader(parentRoot, bc.statedb, prefetch)
 		if err != nil {
 			return nil, err
 		}
-		statedb, err = state.NewWithReader(parentRoot, bc.statedb, reader)
+		statedb, err = state.NewWithReader(parentRoot, bc.statedb, process)
 		if err != nil {
 			return nil, err
 		}
+		// Upload the statistics of reader at the end
+		defer func() {
+			stats := prefetch.GetStats()
+			accountCacheHitPrefetchMeter.Mark(stats.AccountHit)
+			accountCacheMissPrefetchMeter.Mark(stats.AccountMiss)
+			storageCacheHitPrefetchMeter.Mark(stats.StorageHit)
+			storageCacheMissPrefetchMeter.Mark(stats.StorageMiss)
+			stats = process.GetStats()
+			accountCacheHitMeter.Mark(stats.AccountHit)
+			accountCacheMissMeter.Mark(stats.AccountMiss)
+			storageCacheHitMeter.Mark(stats.StorageHit)
+			storageCacheMissMeter.Mark(stats.StorageMiss)
+		}()
+
 		go func(start time.Time, throwaway *state.StateDB, block *types.Block) {
 			// Disable tracing for prefetcher executions.
 			vmCfg := bc.cfg.VmConfig
