44c662407b051b1a2d7ca1fed5111de3a5111567
cui <cuiweixie@gmail.com>
2025-08-25 17:30:51 +0800

    p2p: use slices.Clone (#32428)
    Replaces a helper method with slices.Clone

--- p2p/discover/v5wire/encoding.go
+++ p2p/discover/v5wire/encoding.go
@@ -223,7 +223,7 @@ func (c *Codec) Encode(id enode.ID, addr string, packet Packet, challenge *Whoar
 
 	// Store sent WHOAREYOU challenges.
 	if challenge, ok := packet.(*Whoareyou); ok {
-		challenge.ChallengeData = bytesCopy(&c.buf)
+		challenge.ChallengeData = slices.Clone(c.buf.Bytes())
 		enc, err := c.EncodeRaw(id, head, msgData)
 		if err != nil {
 			return nil, Nonce{}, err
