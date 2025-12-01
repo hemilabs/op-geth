a2648888c74ab2c7e359f6b84414998361ee58b2
Max Sanchez <max@bloq.com>
2025-12-01 03:07:26 -0500

    Logging

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -835,22 +835,31 @@ func ActivePrecompiledContracts(rules params.Rules) PrecompiledContracts {
 func activePrecompiles(rules params.Rules) []common.Address {
 	switch {
 	case rules.IsOptimismIsthmus:
+		log.Info("Isthmus")
 		return PrecompiledAddressesIsthmus
 	case rules.IsOptimismGranite:
+		log.Info("Granite")
 		return PrecompiledAddressesGranite
 	case rules.IsOptimismFjord:
+		log.Info("Fjord")
 		return PrecompiledAddressesFjord
 	case rules.IsPrague:
+		log.Info("Prague")
 		return PrecompiledAddressesPrague
 	case rules.IsCancun:
+		log.Info("Cancun")
 		return PrecompiledAddressesCancun
 	case rules.IsBerlin:
+		log.Info("Berlin")
 		return PrecompiledAddressesBerlin
 	case rules.IsIstanbul:
+		log.Info("Istanbul")
 		return PrecompiledAddressesIstanbul
 	case rules.IsByzantium:
+		log.Info("Byzantium")
 		return PrecompiledAddressesByzantium
 	default:
+		log.Info("Default / Homestead")
 		return PrecompiledAddressesHomestead
 	}
 }
