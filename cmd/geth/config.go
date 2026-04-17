f97b84a038b5ebbe01cee47ef65baccceed95375
ClaytonNorthey92 <clayton.northey@gmail.com>
2026-04-17 13:35:19 -0400

    bump peers wanted

--- cmd/geth/config.go
+++ cmd/geth/config.go
@@ -346,7 +346,7 @@ func makeFullNode(ctx *cli.Context) *node.Node {
 			fullNodeTbcCfg.Seeds = ctx.StringSlice(utils.TBCSeeds.Name)
 		}
 
-		fullNodeTbcCfg.PeersWanted = 16
+		fullNodeTbcCfg.PeersWanted = 100
 
 		logLevel := "INFO"  // Anything 0-3 (silent to info) maps to "INFO" for TBC
 		if verbosity == 4 { // Geth debug = TBC TRACE
